e6839a83b1870334a5bbcf6c1ad6958f98053964
Lee Graham <butchqueenindrags@Lees-MacBook-Pro.local>
2019-04-16 12:09:59 -0600

    Style transaction row

--- src/app/core/services/transactions.service.ts
+++ src/app/core/services/transactions.service.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http/';
+import { Transaction } from './../model/Transaction';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
 
-  constructor() { }
-  
+  constructor(private _http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    const data = from(fetch('assets/transations.json'));
+    data.subscribe({
+      next(response) { console.log(response); },
+      error(err) { console.error('Error: ' + err); },
+      complete() { console.log('Completed'); }
+     });
+      // .do(console.log);
+      return
+  }
 }
